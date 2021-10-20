412e596938b78dceaeed34e71d83f5659e308184
Mathilde SALAUN <24444@student.dorset-college.ie>
2021-10-20 15:40:18 +0100

    Ajoutez des fichiers projet.

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using NPOI.SS.Formula.Functions;
+using System;
+using System.IO;
+
+namespace Test_File_Reader_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = "";
+            string file = "jamaica.txt";
+            string fileToRead = $"{path}/{file}";
+
+            Console.WriteLine($"Reading{fileToRead}");
+            try
+            {
+                using (StreamReader sr=new StreamReader(fileToRead))
+                {
+                    Console.WriteLine($"Starting to Reading {fileToRead}");
+                    string line;
+                    while ((line=sr.ReadLine()) !=null)
+                    {
+                        Console.WriteLine($"\t{line}");
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("The file could not be read.");
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
